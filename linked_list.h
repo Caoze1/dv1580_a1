9d613ac2730b1a79214550b21b404d5351476210
Viktor <viktorfransson03@gmail.com>
2024-10-01 20:43:56 +0200

    fix

--- linked_list.h
+++ linked_list.h
@@ -3,7 +3,7 @@
 
 #include <stdint.h>
 #include <stdlib.h>
-#include <memory_manager.h>
+#include "memory_manager.h"
 
 typedef struct Node {
     uint16_t data; // Stores the data as an unsigned 16-bit integer
