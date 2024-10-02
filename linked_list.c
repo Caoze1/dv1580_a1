5be6045f9fb2eef6d8e5e5fa17f8e6cd6ad2c07b
Viktor <viktorfransson03@gmail.com>
2024-10-02 20:38:50 +0200

    functions

--- linked_list.c
+++ linked_list.c
@@ -42,41 +42,94 @@ void list_insert_before(Node** head, Node* next_node, int data){
 
   node->next = next_node;
 
-  Node* current = *head;
-  while (current->next != next_node) { // go through the list from the front
-    current = current->next;
+  if (*head == next_node){ // if next_node is the first node
+    *head = node;
   }
+  else {
 
-  current->next = node; // then insert the node where the next_node used to be
-
+    Node* current = *head;
+    while (current->next != next_node) { // go through the list from the front
+      current = current->next;
+    }
+    current->next = node; // then insert the node where the next_node used to be
+  }
 };
 
 
 void list_delete(Node** head, int data){
+  Node* current = *head;
+  Node* prev_node = NULL;
 
+  while (current != NULL){
+    if (current->data == data){
+      if (prev_node != NULL){
+        prev_node->next = current->next;
+      }
+
+      mem_free(current);
+    }
+    
+    Node* prev_node = current;
+    current = current->next;
+  }
 };
 
 
 Node* list_search(Node** head, int data){
+  Node* current = *head;
 
-};
-
-
-void list_display(Node** head){
-
+  while (current->data != data){
+    if (current == NULL){
+      printf("data not in list");
+      return 0;
+    }
+    current = current->next;
+  }
+  return current;
 };
 
 
 void list_display_range(Node** head, Node* start_node, Node* end_node){
+  if (start_node == NULL){
+    start_node = *head;
+  }
+  
+  Node* current = start_node;
+  printf("[");
+  while (start_node->next != end_node){ // if end_node is NULL it will still just go to the last one
+    printf("%d", start_node->data);
+    printf(", ");
+    start_node = start_node->next;
+  }
+  printf("%d", start_node->data);
+  printf("]");
+};
 
+
+void list_display(Node** head){
+  list_display_range(head, NULL, NULL);
 };
 
 
 int list_count_nodes(Node** head){
-
+  Node* current = *head;
+  int i = 0;
+  while (current != NULL){
+    i++;
+    current = current->next;
+  }
+  return i;
 };
 
 
 void list_cleanup(Node** head){
-
+  Node* current = *head;
+  Node* next;
+  
+  while (current != NULL){
+    next = current->next;
+    mem_free(current);
+    current = next;
+  }
+  
 };
